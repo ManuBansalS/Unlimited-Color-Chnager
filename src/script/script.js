ed4681f35189ff6b18e33ed4f16727b2a3810572
Manu Bansal <manu03bansal@gmail.com>
2025-07-23 00:36:10 +0530

    create src

---
+++ src/script/script.js
@@ -0,0 +1,32 @@
+function change() {
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++){
+        const col = Math.floor(Math.random()*16)
+        color += hex[col]
+    }
+    return color
+}
+
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+const body  = document.getElementById('body')
+let intervalId = null
+
+start.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(intervalId === null){
+        intervalId = setInterval(() => {
+            body.style.backgroundColor = change()
+        }, 1000)
+
+    }
+})
+
+stop.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(intervalId !== null){
+        clearInterval(intervalId)
+        intervalId = null
+    }
+})
